3ba4806777ab880a8b748e24d74b832227cd1807
connection <Connection>
2024-01-03 15:56:26 +0800

    v240103_1553

--- pkg/sequence/basic.go
+++ pkg/sequence/basic.go
@@ -6,6 +6,19 @@ import (
 	"iPAP/pkg/tool"
 )
 
+const (
+    AminoAcid = "ACDEFGHIKLMNPQRSTVWY"
+)
+
+var Algorithms = []string{
+    "SSE-PSSM",
+}
+
+type Request struct {
+    Method   string `json:"Method"`
+    Sequence string `json:"Sequence"`
+}
+
 func (Seq *Request) IsValid() bool {
 	if Seq.Sequence == "" {
 		return false
