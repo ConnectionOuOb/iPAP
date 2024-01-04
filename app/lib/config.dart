936c434233beaff7ac50ff1d59f8a59c617b3884
Connection || Peter <69660530+Connection2Peter@users.noreply.github.com>
2024-01-04 23:23:06 +0800

    v240104_2323

--- app/lib/config.dart
+++ app/lib/config.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+//# Main page
+Widget firstAlphaHL(String input, double size) {
+  return Row(
+    children: [
+      text(input.substring(0, 1), size, Colors.redAccent, weight: FontWeight.bold),
+      text(input.substring(1), size, Colors.black),
+    ],
+  );
+}
+
 // # Footer
 String footerText1 = "© 2024 - All rights reserved by Connection";
 String footerText2 = "National Yang Ming Chiao Tung University, Taiwan, Roc";
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,6 +2,12 @@ import 'config.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
+class TabItem {
+  String name;
+  IconData? icon;
+
+  TabItem(this.name, this.icon);
+}
 
 void main() {
   runApp(const IPAP());
@@ -32,57 +38,187 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  bool isMobile = false;
+  TextEditingController searchSession = TextEditingController();
+  List<TabItem> tabNames = [
+    TabItem("Home", Icons.home),
+    TabItem("Sequence", Icons.arrow_downward),
+    TabItem("Structure", Icons.arrow_downward),
+    TabItem("Contact", Icons.contacts),
+  ];
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: tabNames.length, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
-    bool isMobile = screenSize.width < 900;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text(
-          ' 𝒊 𝑷 𝑨 𝑷',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    isMobile = screenSize.width < 1000;
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          leadingWidth: 13.w,
+          leading: Container(
+            alignment: Alignment.center,
+            child: Text(
+              ' 𝒊 𝑷 𝑨 𝑷',
+              style: TextStyle(
+                fontSize: isMobile ? 20 : 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
-            Text(
-              'counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          title: TabBar(
+            controller: tabController,
+            isScrollable: true,
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.white,
+            ),
+            indicatorColor: Colors.black,
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            labelPadding: EdgeInsets.zero,
+            dividerColor: Colors.transparent,
+            tabs: tabNames.map((e) {
+              return Container(
+                width: isMobile ? 8.w : 12.w,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (!isMobile) text(e.name, 18, Colors.black, weight: FontWeight.bold),
+                      const SizedBox(width: 5),
+                      if (e.icon != null) Icon(e.icon, size: 20),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+          actions: [
+            SizedBox(
+              width: isMobile ? 30.w : 20.w,
+              height: 40,
+              child: TextField(
+                decoration: const InputDecoration(
+                  isDense: true,
+                  border: OutlineInputBorder(),
+                  hintText: 'Search session',
+                ),
+                controller: searchSession,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+              color: Colors.black,
             ),
           ],
         ),
+        body: TabBarView(
+          controller: tabController,
+          children: [
+            mainPage(),
+            sequence(),
+            sequence(),
+            sequence(),
+          ],
+        ),
+        bottomNavigationBar: Container(
+          height: isMobile ? 100 : 50,
+          color: Colors.black,
+          child: isMobile
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    footerTestStyle(footerText1),
+                    const SizedBox(height: 10),
+                    footerTestStyle(footerText2),
+                  ],
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    footerTestStyle(footerText1),
+                    const SizedBox(width: 20),
+                    footerTestStyle(footerText2),
+                  ],
+                ),
+        ),
       ),
-      bottomNavigationBar: Container(
-        height: isMobile ? 100 : 50,
-        color: Colors.black,
-        child: isMobile
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  footerTestStyle(footerText1),
-                  const SizedBox(height: 10),
-                  footerTestStyle(footerText2),
-                ],
-              )
-            : Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  footerTestStyle(footerText1),
-                  const SizedBox(width: 20),
-                  footerTestStyle(footerText2),
-                ],
-              )
+    );
+  }
+
+  Widget mainPage() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 15.h,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                firstAlphaHL("integrated", 20.sp),
+                const SizedBox(width: 20),
+                firstAlphaHL("Protein", 20.sp),
+                const SizedBox(width: 20),
+                firstAlphaHL("Analysis", 20.sp),
+                const SizedBox(width: 20),
+                firstAlphaHL("Platform", 20.sp),
+              ],
+            ),
+          ),
+          Divider(
+            height: 0,
+            thickness: 1,
+            indent: isMobile ? 10.w : 15.w,
+            endIndent: isMobile ? 10.w : 15.w,
+            color: Colors.purple,
+          ),
+        ],
+        
+      ),
+    );
+  }
+
+  Widget sequence() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 15.h,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                firstAlphaHL("integrated", 20.sp),
+                const SizedBox(width: 20),
+                firstAlphaHL("Protein", 20.sp),
+                const SizedBox(width: 20),
+                firstAlphaHL("Analysis", 20.sp),
+                const SizedBox(width: 20),
+                firstAlphaHL("Platform", 20.sp),
+              ],
+            ),
+          ),
+          Divider(
+            height: 0,
+            thickness: 1,
+            indent: isMobile ? 10.w : 15.w,
+            endIndent: isMobile ? 10.w : 15.w,
+            color: Colors.purple,
+          ),
+        ],
       ),
     );
   }
