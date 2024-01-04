ca99c4511e7f4bfbdad79d602d8d8707b914ddf3
Connection || Peter <69660530+Connection2Peter@users.noreply.github.com>
2024-01-04 18:50:54 +0800

    v240104_1850

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const IPAP());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class IPAP extends StatelessWidget {
+  const IPAP({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ResponsiveSizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          title: 'iPAP',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), useMaterial3: true),
+          home: const MyHomePage(),
+        );
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -32,10 +34,29 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    bool isMobile = screenSize.width < 650;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/images/iPAP_logo.png',
+              fit: BoxFit.contain,
+              height: 32,
+            ),
+            const Text(
+              '2',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Colors.purpleAccent,
+              ),
+            ),
+          ],
+        ),
       ),
       body: Center(
         child: Column(
