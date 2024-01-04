b3f8179ed48d7fb39fb8e5bd4a26e22169ef3241
Connection || Peter <69660530+Connection2Peter@users.noreply.github.com>
2024-01-05 01:31:36 +0800

    v240105_0131

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -188,6 +188,34 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             endIndent: isMobile ? 10.w : 15.w,
             color: Colors.purple,
           ),
+          const SizedBox(height: 30),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: isMobile ? 10.w : 15.w),
+            child: textSelect(
+              "    Introducing our cutting-edge "
+              "Integrated Protein Analysis Platform, a comprehensive "
+              "solution designed to streamline and enhance protein research. "
+              "This platform seamlessly integrates sequence analysis tools "
+              "such as BLAST, Multiple Sequence Alignment (MSA), and Secondary "
+              "Structure Prediction (SSP) to provide a robust foundation for "
+              "understanding protein sequences. In addition to sequence analysis, "
+              "our platform incorporates advanced structure analysis capabilities "
+              "using state-of-the-art technologies like AlphaFold2 and TM-Align. "
+              "AlphaFold2, known for its groundbreaking deep learning approach to "
+              "protein structure prediction, enables accurate and reliable three-dimensional "
+              "structure analysis. TM-Align further enhances structural insights by "
+              "facilitating the comparison and alignment of protein structures. "
+              "This integrated platform not only accelerates the research process but "
+              "also ensures a holistic understanding of proteins, from their primary "
+              "sequences to detailed structural characteristics. Researchers can leverage "
+              "the power of diverse analytical tools within a unified environment, promoting "
+              "efficiency and precision in protein analysis. Stay at the forefront of protein "
+              "research with our Integrated Protein Analysis Platform, where cutting-edge "
+              "technologies converge for a comprehensive and seamless analysis experience.",
+              14.sp,
+              Colors.black,
+            ),
+          ),
         ],
       ),
     );
@@ -211,6 +239,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             endIndent: isMobile ? 10.w : 15.w,
             color: Colors.purple,
           ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: isMobile ? 10.w : 15.w),
+            child: const Text("123"),
+          ),
         ],
       ),
     );
@@ -233,6 +265,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             indent: isMobile ? 10.w : 15.w,
             endIndent: isMobile ? 10.w : 15.w,
             color: Colors.purple,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: isMobile ? 10.w : 15.w),
+            child: const Text("123"),
           ),
         ],
       ),
