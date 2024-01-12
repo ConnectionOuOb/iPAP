a963d31ac9d30225b065e4f5c6c15bd1e007ccb6
Connection || Peter <69660530+Connection2Peter@users.noreply.github.com>
2024-01-12 16:30:06 +0800

    v240112_1630

--- app/lib/pages/result.dart
+++ app/lib/pages/result.dart
@@ -1,6 +1,5 @@
 import '../config.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class PageResult extends StatefulWidget {
@@ -13,6 +12,8 @@ class PageResult extends StatefulWidget {
 }
 
 class _PageResultState extends State<PageResult> with TickerProviderStateMixin {
+  bool endCompute = false;
+
   @override
   void initState() {
     super.initState();
@@ -22,8 +23,37 @@ class _PageResultState extends State<PageResult> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
-        children: [],
+        children: [
+          SizedBox(height: 5.h),
+          Container(
+              margin: widget.isMobile ? EdgeInsets.symmetric(horizontal: 5.w) : EdgeInsets.symmetric(horizontal: 15.w),
+              decoration: BoxDecoration(
+                color: Colors.purple[100],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: endCompute ? result() : computing()),
+        ],
       ),
+    );
+  }
+
+  Widget computing() {
+    return Column(
+      children: [
+        SizedBox(height: 5.h),
+        text('computing', 20.sp, Colors.black, weight: FontWeight.bold),
+        SizedBox(height: 5.h),
+      ],
+    );
+  }
+
+  Widget result() {
+    return Column(
+      children: [
+        SizedBox(height: 5.h),
+        text('Result', 20.sp, Colors.black, weight: FontWeight.bold),
+        SizedBox(height: 5.h),
+      ],
     );
   }
 }
